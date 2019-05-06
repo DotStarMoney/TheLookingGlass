93746e19c6640940c4f6293f0368654f241ec553
DotStarMoney <Batterybat@gmail.com>
2019-05-05 18:40:02 -0700

    Remove comment

--- TheLookingGlass/stagegraph/Index.cs
+++ TheLookingGlass/stagegraph/Index.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-// Make unembed a set content action
-
 namespace TheLookingGlass.StageGraph
 {
     public class Index<ContentType, SharedContentType>
