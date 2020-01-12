73c94308e4ab75d3e0f5e2f68ce4e9ada6e696ad
DotStarMoney <Batterybat@gmail.com>
2020-01-11 21:35:39 -0800

    Started adding Actor

--- TheLookingGlass/actormodel/Actor.cs
+++ TheLookingGlass/actormodel/Actor.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using TheLookingGlass.Core;
 
 namespace TheLookingGlass.ActorModel
 {
-  class Actor
-  {
-  }
+    public abstract class Actor : IAdvanceable, IDeepCloneable
+    {
+        private static readonly BindingFlags CloneBindingConstants = 
+            BindingFlags.Instance | BindingFlags.FlattenHierarchy 
+                | BindingFlags.Public | BindingFlags.NonPublic;
+
+        internal bool enabled = true; 
+
+        void IAdvanceable.Advance(in double deltaT)
+        {
+            if (!enabled)
+            {
+                throw ExUtils.RuntimeException("Called Actor.Advance(...) while disabled.");
+            }
+            Advance(deltaT);
+        }
+
+        protected abstract void Advance(in double deltaT);
+
+        public object DeepClone()
+        {            
+            Actor newActor = (Actor) Activator.CreateInstance(GetType());
+
+            foreach (FieldInfo field in GetType().GetFields(CloneBindingConstants))
+            {
+                object value = field.GetValue(this);
+                if (!Attribute.IsDefined(field, typeof(Uncloneable)))
+                {
+                    field.SetValue(
+                        newActor,
+                        value is IDeepCloneable
+                            ? ((IDeepCloneable)value).DeepClone()
+                            : DeepCloner.Clone(value, field.FieldType));
+
+                }
+            }
+
+            return newActor;
+        }
+    }
 }
+ 
---
+++ TheLookingGlass/core/Uncloneable.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace TheLookingGlass.Core
+{
+    [AttributeUsage(System.AttributeTargets.Field, AllowMultiple = true)]
+    public class Uncloneable : Attribute { }
+}
